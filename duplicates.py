7ca4a13886ab1445d8cb43230bed22bf1ac8e600
sreedevi2906 <67001646+sreedevi2906@users.noreply.github.com>
2020-06-30 09:46:45 +0530

    Add files via upload

--- duplicates.py
+++ duplicates.py
@@ -1,6 +1,6 @@
-2.Write a Python program to remove consecutive duplicates from list
+2.Write a Python program to remove duplicates from a list.
 
-In [64]:
+In [10]:
 mylist=[2,5,5,6,6,9,3]
 prev=None
 newlist=[]
